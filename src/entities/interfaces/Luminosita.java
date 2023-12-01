aebb82548a3f7857c0c918d0d30ab355f58706c3
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2023-12-01 10:56:19 +0100

    Defines abstract class, concrete classes, interfaces

---
+++ src/entities/interfaces/Luminosita.java
@@ -0,0 +1,13 @@
+package entities.interfaces;
+
+public interface Luminosita {
+
+    // ATTRIBUTI
+
+    int luminosita;
+
+    // METODI
+    public void aumentaLuminosita();
+
+    public void diminuisciLuminosita();
+}
